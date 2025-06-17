aedef2e875d519f9a10c954e3ace5b856d6d7fd0
Anirudha Bele <beleanirudha8@gmail.com>
2025-06-17 19:45:38 +0530

    chore: initialize project with Vite and Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+	return (
+		<h1 className="text-orange-500 text-3xl font-bold text-center my-2">
+			Weather Application
+		</h1>
+	);
+}
+
+export default App;
